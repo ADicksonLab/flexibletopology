02f9d603bb118c8f2c201f931dd2589c12355ca1
Nazanin Donyapour <nazanin.donyapour@gmail.com>
2022-01-24 12:14:37 -0500

    add ani model

---
+++ src/flexibletopology/mlmodels/ani.py
@@ -0,0 +1,68 @@
+import os
+import numpy as np
+
+import torch
+from torch import Tensor
+from typing import Tuple, Optional, NamedTuple
+
+from torch import nn
+from torch.jit import Final
+import torchani
+from typing import List
+from .gsg import GSG
+
+
+class Ani(nn.Module):
+
+    def __init__(self, consts_file: str = ''):
+
+        super().__init__()
+        self.is_trainable = False
+        self.consts_file = consts_file
+
+        consts = torchani.neurochem.Constants(self.consts_file)
+        self.aev_computer = torchani.AEVComputer(**consts)
+
+    def forward(self, coordinates: Tensor) -> Tensor:
+
+        species = torch.ones((1, coordinates.shape[0]), dtype=torch.int64)
+        _, aev_signals = self.aev_computer((species,
+                                            coordinates.unsqueeze(0)))
+
+        return aev_signals.squeeze()
+
+
+class AniGSG(nn.Module):
+
+    def __init__(self, max_wavelet_scale: int = 4, radial_cutoff: float = 0.52,
+                 sm_operators: Tuple[bool, bool, bool] = (True, True, True),
+                 consts_file: str = '',
+                 sd_params: Optional[List[List[float]]] = None):
+
+        super().__init__()
+        self.is_trainable = False
+        self.consts_file = consts_file
+        self.max_wavelet_scale = max_wavelet_scale
+        self.radial_cutoff = radial_cutoff
+        self.sm_operators = sm_operators
+        self.sd_params = sd_params
+
+        self.gsg_model = GSG(max_wavelet_scale=self.max_wavelet_scale,
+                             radial_cutoff=self.radial_cutoff,
+                             sm_operators=self.sm_operators,
+                             sd_params=self.sd_params)
+
+        consts = torchani.neurochem.Constants(self.consts_file)
+        self.aev_computer = torchani.AEVComputer(**consts)
+
+    def forward(self, coordinates: Tensor, signals: Tensor) -> Tensor:
+
+        species = torch.ones((1, coordinates.shape[0]), dtype=torch.int64)
+        _, aev_signals = self.aev_computer((species,
+                                            coordinates.unsqueeze(0)))
+
+        signals = torch.cat((aev_signals.squeeze(0),
+                             signals), 1)
+
+        features = self.gsg_model(coordinates, signals)
+        return features
